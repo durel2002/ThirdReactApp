6f862d24153a08b671d8e51c044884ad4cc4cc01
durel2002 <durelhervane@gmail.com>
2023-08-29 23:26:19 +0100

    final app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCounter] = useState(0);
+  const clicked=()=>setCounter(count+1);
+
   return (
    <>
-   <h1> Hello there </h1>
+  <h1> You Have Clicked the button <b><i>{count}</i></b> times</h1>
+  <button onClick={clicked}>Button</button><br/>
+  <input type="text"value={count}/>
    </>
   );
 }
